a0c285e47d88eaac4395adc75243634c9e72b7c9
Nils Jacobsen <nils.jacobsen98@gmail.com>
2023-06-28 20:04:15 +0200

    test: new config

--- inlang.config.js
+++ inlang.config.js
@@ -1,6 +1,6 @@
 export async function defineConfig(env) {
   const { default: i18nextPlugin } = await env.$import(
-    "https://cdn.jsdelivr.net/npm/@inlang/plugin-i18next@2/dist/index.js"
+    "./i18next.js"
   );
   const { default: standardLintRules } = await env.$import(
     "https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@2/dist/index.js"
@@ -10,7 +10,20 @@ export async function defineConfig(env) {
     referenceLanguage: "en",
     plugins: [
       i18nextPlugin({
-        pathPattern: "./website/public/locales/{language}/*.json",
+        pathPattern: {
+          account: "./website/public/locales/{language}/account.json",
+          chat: "./website/public/locales/{language}/chat.json",
+          common: "./website/public/locales/{language}/common.json",
+          dashboard: "./website/public/locales/{language}/dashboard.json",
+          error: "./website/public/locales/{language}/error.json",
+          index: "./website/public/locales/{language}/index.json",
+          labelling: "./website/public/locales/{language}/labelling.json",
+          leaderboard: "./website/public/locales/{language}/leaderboard.json",
+          message: "./website/public/locales/{language}/message.json",
+          stats: "./website/public/locales/{language}/stats.json",
+          tasks: "./website/public/locales/{language}/tasks.json",
+          tos: "./website/public/locales/{language}/tos.json",
+        }
       }),
       standardLintRules(),
     ],
